69039d85b91375bb982e71f05e702104bd8e637c
Thibaut Gatouillat <thibautgatouillat@Thibauts-MacBook-Pro.local>
2020-08-04 20:17:46 +0200

    feat: add router

---
+++ src/components/pages/HomePage/HomePage.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { HomePage } from './HomePage';
+
+
+describe('HomePage', () => {
+  it('should render home page', () => {
+    const { getByText } = render(<HomePage />);
+    const divElement = getByText(/I am on homepage/i);
+    expect(divElement).toBeInTheDocument();
+  });
+});
+
