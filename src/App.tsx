b81d3c6a2f98e0bbd0b6b60daf24b3a8af5264aa
Thibaut Gatouillat <thibaut.gatouillat@gmail.com>
2020-08-05 22:36:49 +0200

    chore: add react-jss

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { HomePage } from "./components/pages/HomePage/HomePage";
 
-import './App.css';
-
 export const App = () => {
   return (
     <Router>
