14902c0a204ad80fc32113dbb1e41aca04ee17da
Thibaut Gatouillat <thibaut.gatouillat@gmail.com>
2020-08-05 08:15:40 +0200

    feat: add router (#5)
    Co-authored-by: Thibaut Gatouillat <thibautgatouillat@Thibauts-MacBook-Pro.local>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { HomePage } from "./components/pages/HomePage/HomePage";
+
 import './App.css';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
-
-export default App;
+};
