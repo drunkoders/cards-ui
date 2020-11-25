e88d736e078f41b4e32ed2b4aa502f855d98f991
guilhermevrs <guilhermevrs@gmail.com>
2020-11-25 20:02:45 +0100

    feat: websockets

---
+++ packages/server/scripts/ws-echo.js
@@ -0,0 +1,19 @@
+// Echo script for websocket calls
+
+const io = require('socket.io-client');
+const socket = io('http://localhost:3000');
+socket.on('connect', function() {
+  console.log('Connected');
+
+  socket.emit('events', { test: 'test' });
+  socket.emit('identity', 0, response => console.log('Identity:', response));
+});
+socket.on('events', function(data) {
+  console.log('event', data);
+});
+socket.on('exception', function(data) {
+  console.log('event', data);
+});
+socket.on('disconnect', function() {
+  console.log('Disconnected');
+});
