ed6608164c032294b43f67791c9b0583bc26da38
guilhermevrs <guilhermevrs@gmail.com>
2020-11-26 14:07:52 +0100

    feat: integrates mongoose

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,6 +1,8 @@
+import * as path from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './models/game/game.module';
@@ -8,6 +10,18 @@ import { GameGatewayModule } from './gateways/game/game.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.ENV_FILE || path.join(__dirname, '../../../.env'),
+      expandVariables: true
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_URL')
+      }),
+      inject: [ConfigService],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
       debug: true,
